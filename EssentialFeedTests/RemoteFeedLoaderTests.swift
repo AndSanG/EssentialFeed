581dd311f0ccb2c91818a3c6e55c920243dda011
Andres Sanchez <and_sg@outlook.com>
2025-01-03 18:23:57 -0500

    url as a parameter

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -6,12 +6,14 @@ import XCTest
 
 class RemoteFeedLoader{
     let client: HTTPClient
-    init(client: HTTPClient){
+    let url: URL
+    init(url: URL, client: HTTPClient){
         self.client = client
+        self.url = url
     }
     func load(){
         // The client instance is injected through constructor
-        client.get(from: URL(string: "https://a-url.com")!)
+        client.get(from:url)
     }
 }
 //abstract class, change to protocol
@@ -28,17 +30,19 @@ class HTTPClientSpy: HTTPClient{
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL(){
+        let url = URL(string: "https://a-url.com")!
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client:client)
+        _ = RemoteFeedLoader(url:url, client:client)
         //Assert that the request is only made when load is changed.
         XCTAssertNil(client.requestedURL)
     }
     
-    func test_init_requestDataFromURL() {
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(url:url!, client: client)
         sut.load()
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 }
 
