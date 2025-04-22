d1df90275e0abc4b1753ec3da9ad6bb19d792df1
Andres Sanchez <and_sg@outlook.com>
2025-04-22 17:54:59 -0500

    add complete to improve readability

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -33,11 +33,12 @@ class RemoteFeedLoaderTests: XCTestCase {
         let (sut, client) = makeSUT()
         
         var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load{capturedErrors.append($0)}
         
         let clientError = NSError(domain: "Test", code: 0)
-        client.completions[0](clientError)
+        client.complete(with: clientError)
         
-        sut.load{capturedErrors.append($0)}
+        
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
     
@@ -60,6 +61,9 @@ class RemoteFeedLoaderTests: XCTestCase {
             requestedURLs.append(url)
         }
         
+        func complete(with error: Error, at index: Int = 0){
+            completions[index](error)
+        }
     }
 }
 
