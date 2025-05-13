30b497b22055b47b5e6bf8b0e63630093e6834af
Andres Sanchez <and_sg@outlook.com>
2025-05-12 21:23:50 -0500

    Delivers conectivity error when receives an error A lot of comments here Names changes to understand. Extend syntax

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -7,17 +7,34 @@
 import Foundation
 
 public protocol HTTPClient{
-    func get(from url :URL)
+    //this function expect a url and a closure
+    //A closure is a self-contained block of code
+    func get(from url :URL, completionGet: @escaping (Error)->Void)
 }
 
 public final class RemoteFeedLoader{
     private let url: URL
     private let client: HTTPClient
+    
+    public enum Error: Swift.Error{
+        case connectivity
+    }
     public init(url: URL, client: HTTPClient){
         self.client = client
         self.url = url
     }
-    public func load(){
-        client.get(from:url)
+    public func load(completionLoad: @escaping (Error) -> Void = {_ in }){
+        /*
+        client.get(from:url){ error in
+            completion(.connectivity)
+        }
+        */
+        client.get(from: url, completionGet: {error in
+            print("load")
+            print("error: ", error)
+            //not passing down the error
+            //here maps http error to domain error
+            completionLoad(.connectivity)
+        })
     }
 }
